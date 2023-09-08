ee6e6e2962d85faf00e8e57ff005d30cf837f83d
Roan al Helly <roan.al.helly.al.helly@hva.nl>
2023-09-08 10:56:12 +0200

    landingspagina prima voor nu, registreren en inlog pagina

--- dokkie_database.sql
+++ dokkie_database.sql
@@ -5,15 +5,15 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema Dokkie
+-- Schema pb1a2324_hellyr_live
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Dokkie` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `Dokkie` ;
+CREATE SCHEMA IF NOT EXISTS `pb1a2324_hellyr_live` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `pb1a2324_hellyr_live` ;
 
 -- -----------------------------------------------------
--- Table `Dokkie`.`Event`
+-- Table `pb1a2324_hellyr_live`.`Event`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Dokkie`.`Event` (
+CREATE TABLE IF NOT EXISTS `pb1a2324_hellyr_live`.`Event` (
   `eventId` INT NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(100) NOT NULL,
   `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -23,9 +23,9 @@ DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
--- Table `Dokkie`.`User`
+-- Table `pb1a2324_hellyr_live`.`User`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Dokkie`.`User` (
+CREATE TABLE IF NOT EXISTS `pb1a2324_hellyr_live`.`User` (
   `userId` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
@@ -34,9 +34,9 @@ CREATE TABLE IF NOT EXISTS `Dokkie`.`User` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Dokkie`.`Participant`
+-- Table `pb1a2324_hellyr_live`.`Participant`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Dokkie`.`Participant` (
+CREATE TABLE IF NOT EXISTS `pb1a2324_hellyr_live`.`Participant` (
   `eventId` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `userId` INT NULL,
@@ -45,12 +45,12 @@ CREATE TABLE IF NOT EXISTS `Dokkie`.`Participant` (
   INDEX `user_idx` (`userId` ASC),
   CONSTRAINT `fk_participant_event`
     FOREIGN KEY (`eventId`)
-    REFERENCES `Dokkie`.`Event` (`eventId`)
+    REFERENCES `pb1a2324_hellyr_live`.`Event` (`eventId`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
   CONSTRAINT `fk_participant_user`
     FOREIGN KEY (`userId`)
-    REFERENCES `Dokkie`.`User` (`userId`)
+    REFERENCES `pb1a2324_hellyr_live`.`User` (`userId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -58,9 +58,9 @@ DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
--- Table `Dokkie`.`Payment`
+-- Table `pb1a2324_hellyr_live`.`Payment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Dokkie`.`Payment` (
+CREATE TABLE IF NOT EXISTS `pb1a2324_hellyr_live`.`Payment` (
   `paymentId` INT NOT NULL AUTO_INCREMENT,
   `datePaid` DATE NOT NULL,
   `description` VARCHAR(100) NOT NULL,
@@ -71,12 +71,12 @@ CREATE TABLE IF NOT EXISTS `Dokkie`.`Payment` (
   INDEX `participant_idx` (`eventId` ASC, `name` ASC),
   CONSTRAINT `fk_payment_participant`
     FOREIGN KEY (`eventId` , `name`)
-    REFERENCES `Dokkie`.`Participant` (`eventId` , `name`)
+    REFERENCES `pb1a2324_hellyr_live`.`Participant` (`eventId` , `name`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
-USE `Dokkie` ;
+USE `pb1a2324_hellyr_live` ;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
