190c0b9dca63fc421b5a2714585973403b3ecae3
Roan al Helly <roan.al.helly.al.helly@hva.nl>
2023-09-15 11:14:39 +0200

    landinspagina met grid

--- src/javascript.js
+++ src/javascript.js
@@ -1,10 +1,18 @@
-import "./hboictcloud-config";
+// import "./hboictcloud-config";
 
-/**
- * Entry point
- */
-function app() {
-    console.log("Hello world from JavaScript!");
+// /**
+//  * Entry point
+//  */
+// function app() {
+//     console.log("Hello world from JavaScript!");
+// }
+function myFunction() {
+    const x = document.getElementById("mynavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
 }
 
 app();
