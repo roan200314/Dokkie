1a85485e24ea5e55eaf5e4fdef15e520aafa4a65
Roan al Helly <roan.al.helly.al.helly@hva.nl>
2023-09-26 13:59:39 +0200

    css added op de sites

--- src/register.ts
+++ src/register.ts
@@ -1,13 +1,13 @@
 import { runQuery } from "./utils/queryutil";
 
-function register() :void{
+// function register() :void{
 
-}
+// }
 
-async function db(params:type): Promise<void> {
+// async function db(params:type): Promise<void> {
 
-    const query: string = "INSERT INTO payment (, email, password, username) VALUES (?)";
-    const values: string[] = [user.username, user.email, user.password, user.username];
+//     const query: string = "INSERT INTO payment (, email, password, username) VALUES (?)";
+//     const values: string[] = [user.username, user.email, user.password, user.username];
 
-await runQuery(query, values);
-}
+// await runQuery(query, values);
+// }
--- src/uitje.ts
+++ src/uitje.ts
@@ -1,27 +1,40 @@
 import { runQuery } from "./utils/queryutil";
 
-// // Function to send the data to the database
-// // Get the form input values
-// const emailInput = document.getElementById('email') as HTMLInputElement | null;
-// const uitjeInput = document.getElementById('uit') as HTMLInputElement | null;
-// const prijsInput = document.getElementById('prijs') as HTMLInputElement | null;
+function uitje() :void {
+    
+    const uitje: HTMLFormElement = document.getElementById("formUitje") as HTMLFormElement;
+    uitje.addEventListener("submit", async (event) => { 
 
-// if (emailInput && uitjeInput && prijsInput) {
-//   const email = emailInput.value;
-//   const uitje = uitjeInput.value;
-//   const prijs = prijsInput.value;
+        // Remove error message if exists
+        document.getElementById("error")?.remove();
 
-//   // Create an object with the data
-//   const data = {
-//     email,
-//     uitje,
-//     prijs,
-//     apiKey: 'pb1a2324_hellyr.RJgG1hyQcU20fHrQ', // Your API key
-//     username: 'pb1a2324_hellyr', // Your username
-//   };
+        // Get form data
+        const formData: FormData = new FormData(uitje);
+        const formDataObject: { [key: string]: string } = {};
 
-//   // ... rest of the code remains the same
-// } else {
-//   console.error('One or more input fields not found');
+        // Convert formData to a JavaScript object
+        formData.forEach((value, key) => {
+            formDataObject[key] = value as string;
+    });
+
+    const email: string = formDataObject["email"];
+    const uit: string = formDataObject["uit"];
+    const prijs: string = formDataObject["prijs"];
+
+
+
+
+});
+
+}
+
+// async function db(params:type) {
+
+//     const query: string = "INSERT INTO payment (name, email, password, salt) VALUES (?)";
+//     const values: string[] = [user.username, user.email, user.password, user.salt];
+
+// await runQuery(query, values);
 // }
 
+
+uitje();
