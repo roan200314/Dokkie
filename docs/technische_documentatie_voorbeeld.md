34828b0d1cfa8b100454fabd19a34f6cee240a45
Roan al Helly <roan.al.helly.al.helly@hva.nl>
2023-10-03 14:54:22 +0200

    uitje op het scherm

--- docs/technische_documentatie_voorbeeld.md
+++ docs/technische_documentatie_voorbeeld.md
@@ -2,5 +2,33 @@
 
 Beschrijf hier wat je geleerd hebt op gebied van de beroepscriteria uit de opdracht(zie de Wiki). Je kunt stukken code toevoegen, maar ook afbeeldingen, diagrammen. Je docent geeft je nog extra informatie over de technische documentatie!
 
-# retrospective:
+# documentatie
+
+Eerste week heb ik vooral besteed aan de landingspagina. Zo was ik met losse pols begonnen met programmeren. Ik had al eerst een grid container gemaakt om mijn gegevens in te zetten. Daarna was ik verder gegaan met de CSS van de website. Daarna had ik de connectie gemaakt met de database en de database geimporteerd in PHPmyAdmin. Zo was ik klaar met mijn eerste userstory.
+
+Tweede week ben ik bezig geweest met het responisive maken en teksten op het scherm te zetten. 
+Ook ben ik begonnen met een pagina maken om een uitje op het scherm te zetten. 
+
+- Begonnen met logica
+
+Ik heb toen de typscript documenten aangemaakt om te gaan beginnen met logica. Dit hbe ik aangepakt door eerst te leren te komen wat voor taal Typescript is. Zo heb ik eerst video's bekeken en online gekeken waar Typescript op lijkt en hoe je basis functies maakt. 
+
+Ik had toen een 'utils' mapje aangemaakt waarin ik een queryutil.ts heb toegevoegd. Met dit als code:
+
+<code>
+import { api } from "@hboictcloud/api";
+import "../hboictcloud-config";
+
+export async function runQuery(query: string, params?: Array<any>): Promise<Array<any> | undefined> {
+    try {
+        return (await api.queryDatabase(query, params)) as Array<any>;
+    } catch (error) {
+        console.error(error);
+    }
+
+    return undefined;
+}
+</code>
+
+Met deze code heb ik een maniet gemaakt waarin ik alleen de code hoef aan te roepen om een query in mijn database te zetten.
 
--- src/uitje.ts
+++ src/uitje.ts
@@ -8,36 +8,67 @@ knopUitjeZien.addEventListener("click", laatZien);
 
 
 async function zetIn(): Promise<void> {
+  //een uitje aanmaken
   const emailInput: HTMLInputElement | null = document.getElementById("email");
   const uitjeInput: HTMLInputElement | null = document.getElementById("uit");
   const prijsInput: HTMLInputElement | null = document.getElementById("prijs");
-  // Get the form input values
-  
+
+  // Form input velden opslaan
     const email: string = emailInput.value;
     const uitje: string = uitjeInput.value;
     const prijs: string = prijsInput.value;
     if (emailInput && uitjeInput && prijsInput) {
   
-      // Log the form data to the console
+      // data in console zetten om te checken
       console.log("Email:", email);
       console.log("Uitje:", uitje);
       console.log("Prijs:", prijs);
     } else {
       console.error("One or more input fields not found");
     }
-    
-    await runQuery("INSERT INTO event (description) VALUES (?)", [uitje]);
 
-    
+    //inserten in database
+    await runQuery("INSERT INTO event (description, price) VALUES (?)", [uitje, prijs]);   
   }
 
-
   async function laatZien(): Promise<void> {
+    
+    //data opslaan in de div
+    const data: HTMLElement | null = document.getElementById("uitjeTest");
+    
+    
+      const div: HTMLElement | null = document.createElement("div");
+      //paragraaf aanmaken
+      const paragraph: HTMLElement | null = document.createElement("p");
+      const paragraph2: HTMLElement | null = document.createElement("p");
+      //knop aanmaken
+      const buttonJoin: HTMLElement | null = document.createElement("button");
+      
+      
+      // runquery oproepen en data ophalen door een const aan te maken die alles kan pakken
+      const resultaat: any[] | undefined = await runQuery("SELECT * FROM event");
 
-    await runQuery("SELECT * FROM event");
 
-      console.log();
-   }
+      if (resultaat && resultaat.length > 0) {
+        resultaat.forEach((row: any) => {
+        paragraph.textContent = `Event Description: ${row.description}`;
+        paragraph2.textContent = `Event price: ${row.price}`;
+        buttonJoin.textContent = "Join dit uitje!";
+
+        data.appendChild(div);
+        data.appendChild(paragraph);
+        data.appendChild(paragraph2);
+        data.appendChild(buttonJoin);
+
+
+        });
+
+
+      console.log(resultaat);
+    
+    }
+  }
+
   
   
 
